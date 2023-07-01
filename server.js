01f216cc296a05b782d89cb2e2239289858013bb
rakesh0513 <drakeshkumar0513@gmail.com>
2023-07-01 21:56:57 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// import module express body-parser cors
+let express = require('express')
+let bodyparser = require('body-parser')
+let cors = require('cors')
+//create rest object
+let app = express()
+//set JSON as MIME type
+app.use(bodyparser.json())
+//client is not sending from data -> encoding JSON
+app.use(bodyparser.urlencoded({extended : false}))
+// enable CORS -> Cross Origine resource sharing -> communication among various ports
+app.use(cors())
+//create port
+let port = process.env.PORT || 8080
+//import fetch insert update delete modules
+let fetch = require('./fetch/fetch')
+let insert = require('./insert/insert')
+let update = require('./update/update')
+let remov = require('./delete/delete')
+//use above modules
+app.use("/fetch", fetch)
+app.use("/insert", insert)
+app.use("/update", update)
+app.use("/delete", remov)
+//assign port no
+app.listen(port, () => {
+    console.log("Server listening port no:- ", port)
+})
+/*
+    >node server
+    Test following URLs with postman
+    http://localhost:8080/fetch     (get)
+    http://localhost:8080/insert    |
+    http://localhost:8080/update    |(post)
+    http://localhost:8080/delete    |
+
+
+    body -> raw -> json
+
+
+*/
